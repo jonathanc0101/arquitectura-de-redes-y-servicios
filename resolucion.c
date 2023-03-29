8ac3ad780c420e0ddf13471d177c49e2da0ff119
JC <jonathancavia0101@gmail.com>
2023-03-29 17:40:37 -0300

    resultado final en resolucion.c

--- resolucion.c
+++ resolucion.c
@@ -9,28 +9,31 @@ int main(int argc, char** argv) {
     double scalar = 2;
     double vector[VECTOR_SIZE];
     double result[VECTOR_SIZE];
-    
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if(VECTOR_SIZE < size){
+        if(my_rank == 0){
+            perror("\nWe have more processes than the size of the array, aborting\n");
+        }
+        exit(EXIT_FAILURE);
+    }
 
     // Initialize the vector
     for (i = 0; i < VECTOR_SIZE; i++) {
         vector[i] = i + 1;
     }
 
-    // Divide the vector into equal-sized chunks for each process, leave one of the processes
-    // for a different chunk size
-    int chunk_size = VECTOR_SIZE / (size -1);
-    // En caso de que la división entera nos de algún numero que no se corresponde con el tamaño
-    // inicial, calculamos manualmente el tamaño del ultimo chunk
-    int last_chunk_size = VECTOR_SIZE -  chunk_size * size + chunk_size ;
-
-    printf("\nchunk size: %d\n",chunk_size);
-    printf("\nlast chunk size: %d\n",last_chunk_size);
+    // Divide the vector into equal-sized chunks for each process
+    int chunk_size = VECTOR_SIZE / size;
+    // Except for the last chunk, if it happens to be 0
+    int last_chunk_size = VECTOR_SIZE - chunk_size * size;
 
     double local_vector[chunk_size];
     double local_result[chunk_size];
+
 
     MPI_Scatter(vector, chunk_size, MPI_DOUBLE, local_vector, chunk_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
@@ -44,6 +47,16 @@ int main(int argc, char** argv) {
 
     // Print the results from process 0
     if (my_rank == 0) {
+
+        if(last_chunk_size > 0){
+            //we still have some work to do
+            for (i = VECTOR_SIZE - last_chunk_size - 1; i < VECTOR_SIZE; i++)
+            {
+                result[i] = vector[i] * scalar;
+            }
+            
+        }
+
         printf("Original vector:\n");
         for (i = 0; i < VECTOR_SIZE; i++) {
             printf("%f ", vector[i]);
