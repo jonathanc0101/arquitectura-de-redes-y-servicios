9f84b81d11358506d1acf373555c0d4cf8e614b5
JC <jonathancavia0101@gmail.com>
2023-03-31 18:43:22 -0300

    arreglando tp2

---
+++ codelab-friendlychat-web/web/src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDl85SH6xjoBkIPu8UwPHV1S22HEmvkq4s",
+  authDomain: "tp2-arq-redes-servicios.firebaseapp.com",
+  projectId: "tp2-arq-redes-servicios",
+  storageBucket: "tp2-arq-redes-servicios.appspot.com",
+  messagingSenderId: "914786110860",
+  appId: "1:914786110860:web:cc0bc305c95ec11291daa9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
