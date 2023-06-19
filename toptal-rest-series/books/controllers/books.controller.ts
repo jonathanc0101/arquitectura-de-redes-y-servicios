f29fd2a309b07d50c06b90e5f949b4b224081c21
jonathanc0101 <joanthancavia0101@gmail.com>
2023-06-19 16:28:11 -0300

    need to test

--- toptal-rest-series/books/controllers/books.controller.ts
+++ toptal-rest-series/books/controllers/books.controller.ts
@@ -5,6 +5,20 @@ import debug from "debug";
 const log: debug.IDebugger = debug("app:books-controller");
 
 class BooksController{
+  async patch(req: express.Request, res: express.Response) {
+    log(await booksService.patchById(req.body.id, req.body));
+    res.status(204).send();
+  }
+
+  async put(req: express.Request, res: express.Response) {
+    log(await booksService.putById(req.body.id, req.body));
+    res.status(204).send();
+  }
+
+  async removeBook(req: express.Request, res: express.Response) {
+    log(await booksService.deleteById(req.body.id));
+    res.status(204).send();
+  }
 
 async listBooks(req: express.Request, res: express.Response) {
     const books = await booksService.listBooks(req.body.id)
